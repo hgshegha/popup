8e1222bd0649e3ff52bc72b47905a2312e3db7ef
hgshegha <73647697+hgshegha@users.noreply.github.com>
2022-12-17 18:16:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+$('.open-popup-client').click(function(e) {
+    e.preventDefault();
+    $('.pb').fadeIn(900);
+    $('html').addClass('noScroll');
+})
+
+$(".fa-times").click(function(e){
+    $('.pb').fadeOut(900);
+    $('html').removeClass('noScroll')
+})
+
 
